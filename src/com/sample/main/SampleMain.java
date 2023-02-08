92615d3fcd2f3407129995c8426e5cac8b338170
anna1987S <annamalai.senthilvel@chainsys.com>
2023-02-08 14:33:41 +0530

    commit main

--- src/com/sample/main/SampleMain.java
+++ src/com/sample/main/SampleMain.java
@@ -3,8 +3,7 @@ package com.sample.main;
 public class SampleMain {
 
 	public static void main(String[] args) {
-		System.out.println("GIT TEST1");
-
+		System.out.println("This is my git java program");
 	}
 
 }
